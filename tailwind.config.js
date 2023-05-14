e9c333db98cac6266d69dd658887c9706494d720
Jan Chalupa <janchalupa@outlook.cz>
2023-05-14 20:02:03 +0200

    fix

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
     content: [
-        "./**/*.{js,ts,jsx,tsx}",
+        "./**/*!(node_modules)/*.{js,ts,jsx,tsx}",
         "./node_modules/@honzachalupa/design-system/**/*.{js,ts,jsx,tsx}",
     ],
     theme: {
